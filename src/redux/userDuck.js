e13dd6daee60229b76f03c54bfd846b5eee240d1
Fedeaguilera94 <aguilerafederico94@gmail.com>
2021-11-06 18:51:12 -0300

    config-redux

---
+++ src/redux/userDuck.js
@@ -0,0 +1,15 @@
+// constants
+let initialData = {
+  loggedIn: false,
+};
+let LOGIN = "LOGIN";
+//reducer
+export default function reducer(state = initialData, action) {
+  switch (action.type) {
+    case LOGIN:
+    default:
+      return state;
+  }
+}
+
+// action
